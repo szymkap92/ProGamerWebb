caefb41538999c508c76252fa0f3cce431934eb4
Szymon Kaput <szymonkaput123@gmail.com>
2024-12-08 16:09:06 +0100

    Update navbar funcionality

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const nav = document.querySelector('.navbar')
-	const allNavItems = document.querySelectorAll('.nav-link')
-	const navList = document.querySelector('.navbar-collapse')
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelector(".navbar");
+  const allNavItems = document.querySelectorAll(".nav-link");
+  const navList = document.querySelector(".navbar-collapse");
 
-	function addShadow() {
-		if (window.scrollY >= 100) {
-			nav.classList.add('shadow-bg')
-		} else {
-			nav.classList.remove('shadow-bg')
-		}
-	}
+  function addShadow() {
+    if (window.scrollY >= 0) {
+      nav.classList.add("shadow-bg");
+    } else {
+      nav.classList.remove("shadow-bg");
+    }
+  }
 
-	allNavItems.forEach(item =>
-		item.addEventListener('click', () => {
-			navList.classList.remove('show')
-		})
-	)
+  allNavItems.forEach((item) =>
+    item.addEventListener("click", () => {
+      navList.classList.remove("show");
+    })
+  );
+  document.addEventListener("click", function (event) {
+    if (!nav.contains(event.target) && !navList.contains(event.target)) {
+      navList.classList.remove("show");
+    }
+  });
 
-	
-
-	window.addEventListener('scroll', addShadow)
-})
-
+  window.addEventListener("scroll", addShadow);
+});
